8257bc9db9b22002d974fa0b022714c1037ba8bd
temaxuck <gorropand@gmail.com>
2024-02-03 11:18:08 +0300

    Fix wrong file module description

---
+++ analyzer/__init__.py
@@ -0,0 +1,19 @@
+"""
+REST API service for tracking and analyzing promotions.
+"""
+
+__author__ = "Temax"
+__maintainer__ = "Temax"
+
+__email__ = "gorropand@gmail.com"
+__version__ = "0.0.1"
+__license__ = "No license"
+
+
+__all__ = [
+    '__author__',
+    '__maintainer__',
+    '__email__',
+    '__version__',
+    '__license__',
+]
