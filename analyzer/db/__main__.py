5498f2645e2ebfca669192ef88ab534af8913e64
temaxuck <gorropand@gmail.com>
2024-02-03 10:59:33 +0300

    Create basic structure and docstring for the analyzer module

---
+++ analyzer/db/__main__.py
@@ -0,0 +1,16 @@
+"""
+REST API service for tracking and analyzing promotions.
+"""
+
+__author__ = "Temax"
+__maintainer__ = "Temax"
+
+__email__ = "gorropand@gmail.com"
+__version__ = "0.0.1"
+
+__all__ = [
+    '__author__',
+    '__maintainer__',
+    '__email__',
+    '__version__',
+]
