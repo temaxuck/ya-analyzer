17280b9174a27c986ca38b154d8695cf2cf75471
temaxuck <gorropand@gmail.com>
2024-02-04 18:08:02 +0300

    Update `Config` with new value `ENV_VAR_PREFIX`

--- analyzer/config.py
+++ analyzer/config.py
@@ -1,6 +1,11 @@
+# Default Config
 class Config:
+
     DEBUG = False
     TESTING = False
+
     DATABASE_URI = "postgresql://admin:admin@localhost/analyzer"
     API_HOST = "0.0.0.0"
     API_PORT = 8080
+
+    ENV_VAR_PREFIX = "ANALYZER_"
