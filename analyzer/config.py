8c70e5a8cd4952f469a5c969e4e190351cf1300c
temaxuck <gorropand@gmail.com>
2024-02-04 19:13:53 +0300

    Update config with new value `MAX_REQUEST_SIZE`

--- analyzer/config.py
+++ analyzer/config.py
@@ -1,3 +1,6 @@
+MEGABYTE = 1024 * 1024
+
+
 # Default Config
 class Config:
     """
@@ -14,13 +17,14 @@ class Config:
     # API variables
     API_HOST = "0.0.0.0"
     API_PORT = 8080
+    MAX_REQUEST_SIZE = 64 * MEGABYTE
 
     # Database variables
     DATABASE_URI = "postgresql://admin:admin@localhost/analyzer"
 
     # env parser variables
     ENV_VAR_PREFIX = "ANALYZER_"
-    
+
     # Logging variables
     LOG_LEVEL = "info"
     LOG_FORMAT = "color"
