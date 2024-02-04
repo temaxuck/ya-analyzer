88014b4307b4c0ebb2c4a6413f2f51b4d9cffeea
temaxuck <gorropand@gmail.com>
2024-02-04 12:18:36 +0300

    Implement data schema

--- analyzer/db/schema.py
+++ analyzer/db/schema.py
@@ -1,5 +1,18 @@
-from sqlalchemy import MetaData
+from enum import Enum, unique
+from sqlalchemy import (
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    ForeignKey,
+    String,
+    Date,
+    Enum as pgEnum,
+    ForeignKeyConstraint,
+)
 
+
+# Naming Convention for tables and constraints
 convention = {
     "all_column_names": lambda constraint, table: "_".join(
         [column.name for column in constraint.columns.values()]
@@ -17,3 +30,42 @@ convention = {
 }
 
 metadata = MetaData(naming_convention=convention)
+
+
+@unique
+class Gender(Enum):
+    male = "male"
+    female = "female"
+
+
+import_table = Table("import", metadata, Column("import_id", Integer, primary_key=True))
+
+citizen_table = Table(
+    "citizen",
+    metadata,
+    Column("import_id", Integer, ForeignKey("import.import_id"), primary_key=True),
+    Column("citizen_id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("birth_date", Date, nullable=False),
+    Column("gender", pgEnum(Gender, name="gender"), nullable=False),
+    Column("town", String, nullable=False, index=True),
+    Column("street", String, nullable=False),
+    Column("building", String, nullable=False),
+    Column("apartment", Integer, nullable=False),
+)
+
+relation_table = Table(
+    "relation",
+    metadata,
+    Column("import_id", Integer, primary_key=True),
+    Column("citizen_id", Integer, primary_key=True),
+    Column("relative_id", Integer, primary_key=True),
+    ForeignKeyConstraint(
+        ("import_id", "citizen_id"),
+        ("citizen.import_id", "citizen.citizen_id"),
+    ),
+    ForeignKeyConstraint(
+        ("import_id", "relative_id"),
+        ("citizen.import_id", "citizen.citizen_id"),
+    ),
+)
