60251f0d817c6376f1cb040b7eb0f1603c8cbaeb
temaxuck <gorropand@gmail.com>
2024-02-05 14:19:31 +0300

    Introduce database setup function `setup_pg`

---
+++ analyzer/utils/pg.py
@@ -0,0 +1,29 @@
+import logging
+
+from aiohttp import web
+from asyncpgsa import PG
+from configargparse import Namespace
+
+logger = logging.getLogger(__name__)
+CENSORED = "***"
+
+
+async def setup_pg(app: web.Application, args: Namespace):
+    db_info = args.pg_url.with_password(CENSORED)
+    logger.info(f"Connecting to database: {db_info}")
+
+    app["pg"] = PG()
+    await app["pg"].init(
+        str(args.pg_url),
+        min_size=args.pg_pool_min_size,
+        max_size=args.pg_pool_max_size,
+    )
+    await app["pg"].fetchval("SELECT 1")
+    logger.info(f"Connected to database: {db_info}")
+
+    try:
+        yield
+    finally:
+        logger.info(f"Disconnecting from database: {db_info}")
+        await app["pg"].pool.close()
+        logger.info(f"Disconnected from database: {db_info}")
