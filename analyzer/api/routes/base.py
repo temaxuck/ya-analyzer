ec008504dddb676a9b617cf3a5efe47d79daa324
temaxuck <gorropand@gmail.com>
2024-02-06 15:58:08 +0300

    Add `app` getter to `BaseView` class

--- analyzer/api/routes/base.py
+++ analyzer/api/routes/base.py
@@ -9,5 +9,9 @@ class BaseView(web.View):
     URL_PATH: str
 
     @property
+    def app(self) -> web.Application:
+        return self.request.app
+
+    @property
     def pg(self) -> "Pool":
         return self.request.app["pg"]
