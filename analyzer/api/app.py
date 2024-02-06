65b2589b5fd303b64dd004c8ab428f1b6a98077b
temaxuck <gorropand@gmail.com>
2024-02-06 07:42:29 +0300

    Change registering routes method

--- analyzer/api/app.py
+++ analyzer/api/app.py
@@ -1,8 +1,12 @@
+import logging
+
 from aiohttp import web
 from configargparse import Namespace
 
-from analyzer.api.routes import routes
+from analyzer.api.routes import ROUTES
 from analyzer.config import Config
+
+logger = logging.getLogger(__name__)
 
 
 def init_app(args: Namespace, cfg: Config) -> web.Application:
@@ -12,6 +16,11 @@ def init_app(args: Namespace, cfg: Config) -> web.Application:
 
     app = web.Application(client_max_size=args.api_max_request_size)
     app["config"] = cfg
-    app.add_routes(routes)
+
+    for route in ROUTES:
+        logger.debug(f"Registering route {route} as {route.URL_PATH}")
+        app.router.add_route("*", route.URL_PATH, route)
+
+    # app.add_routes(routes)
 
     return app
--- analyzer/api/routes/__init__.py
+++ analyzer/api/routes/__init__.py
@@ -1,9 +1,19 @@
+"""
 from aiohttp import web
 
+# Not using `routes` anymore
 routes = web.RouteTableDef()
 
 
+# Test handler
 @routes.get(r"/next/{num:\d+}")
 async def get_next_num(request: web.Request) -> web.Response:
     num = int(request.match_info["num"])
     return web.Response(text=f"Next number for number {num} is: {num + 1}")
+"""
+
+ROUTES = tuple()
+
+# from .imports import ImportsView
+
+# ROUTES = (ImportsView,)
