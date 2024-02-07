85397895423554fdeb2c7e860110f9d5e06260ea
temaxuck <gorropand@gmail.com>
2024-02-07 06:38:58 +0300

    Create `BaseImportView` base view class for views that acquire `import_id`

--- analyzer/api/routes/base.py
+++ analyzer/api/routes/base.py
@@ -15,3 +15,22 @@ class BaseView(web.View):
     @property
     def pg(self) -> "Pool":
         return self.request.app["pg"]
+
+
+class BaseImportView(BaseView):
+    @property
+    def import_id(self) -> int:
+        return int(self.request.match_info.get("import_id"))
+
+    async def check_if_import_exists(self) -> bool:
+        async with self.pg.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(
+                    "SELECT import_id FROM import WHERE import_id=%s", self.import_id
+                )
+                import_id = await cur.fetchone()
+
+                if not import_id:
+                    raise web.HTTPNotFound()
+
+        return True
