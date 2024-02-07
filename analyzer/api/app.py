54ae0b313447589e46eb1a0b49323e912a31e6ca
temaxuck <gorropand@gmail.com>
2024-02-07 08:46:58 +0300

    Register payload handlers

--- analyzer/api/app.py
+++ analyzer/api/app.py
@@ -1,10 +1,13 @@
 import logging
 
-from aiohttp import web
+from aiohttp import web, PAYLOAD_REGISTRY
 from aiohttp_apispec import setup_aiohttp_apispec, validation_middleware
 from configargparse import Namespace
+from types import AsyncGeneratorType, MappingProxyType
+from typing import AsyncIterable, Mapping
 
 from analyzer.api.routes import ROUTES
+from analyzer.api.payload import AsyncGenJsonListPayload, JsonPayload
 from analyzer.config import Config
 from analyzer.utils.pg import setup_pg
 
@@ -23,12 +26,18 @@ def init_app(args: Namespace, cfg: Config) -> web.Application:
     app["config"] = cfg
     app.cleanup_ctx.append(lambda _: setup_pg(app, args=args))
 
+    # app.add_routes(routes)
     for route in ROUTES:
         logger.debug(f"Registering route {route} as {route.URL_PATH}")
         app.router.add_route("*", route.URL_PATH, route)
 
     setup_aiohttp_apispec(app=app, title="Ya-analyzer API", swagger_path="/")
 
-    # app.add_routes(routes)
+    PAYLOAD_REGISTRY.register(
+        AsyncGenJsonListPayload, (AsyncGeneratorType, AsyncIterable)
+    )
+    PAYLOAD_REGISTRY.register(
+        JsonPayload, (Mapping, MappingProxyType)
+    )
 
     return app
